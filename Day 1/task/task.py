62a3f78a3b86e5c386505cd4d059fcb06be78cb3
RISHITHA <rralle2006@gmail.com>
2026-05-16 13:35:08 +0530

    band name generator

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("----- Welcome to Band Name generator Project -----")
+city = input("Enter city you grew up in\n")
+pet_name = input("Enter your pet name\n")
+print("Your Band name:" + city + " " + pet_name)
